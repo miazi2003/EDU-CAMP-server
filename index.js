97f194276e4a4181f72755e94bf4e3aa0dd7e489
miazi2003 <yeasinmiazi1997@gmail.com>
2025-06-11 12:55:45 +0600

    get and assignment delete api is ready without verify

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 3000
-const cors = require("cors")
-require('dotenv').config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3crt5al.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -16,40 +18,67 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
-
-
 async function run() {
-
-
-
-
-  
   try {
+    const database = client.db("edu-camp");
+    const createCollection = database.collection("assignment-create");
+
+
+
+
+
+
+
+
+
+    app.get("/createAssignment", async (req, res) => {
+      const cursor = createCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.post('/createAssignment' , async(req,res)=>{
+      const newAssignment = req.body;
+      
+      const result = await createCollection.insertOne(newAssignment) 
+      res.send(result)
+    })
+
+    app.post('/deleteAssignment/:id' , async(req , res)=>{
+      const id = req.params.id
+      const userEmail = req.body.email;
+      console.log(userEmail)
+      const query = {
+        _id : new ObjectId(id),
+        email : userEmail
+      }
+      const result = await createCollection.deleteOne(query)
+      res.send(result)
+    } )
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  console.log("server running");
+  res.send("server is cooking for eduCamp");
+});
 
-
-
-
-app.get('/' , (req,res)=>{
-console.log("server running")
-res.send("server is cooking for eduCamp")
-})
-
-
-
-app.listen(port , ()=>{console.log("server running on " , port)})
+app.listen(port, () => {
+  console.log("server running on ", port);
+});
