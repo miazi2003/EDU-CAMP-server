79907b582d96bb43f54f8c3f8496de13e1ea3792
miazi2003 <yeasinmiazi1997@gmail.com>
2025-06-14 23:00:22 +0600

    put method of update assignment added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3crt5al.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,14 +24,7 @@ async function run() {
   try {
     const database = client.db("edu-camp");
     const createCollection = database.collection("assignment-create");
-
-
-
-
-
-
-
-
+    const submittedCollection = database.collection("assignment-submit");
 
     app.get("/createAssignment", async (req, res) => {
       const cursor = createCollection.find();
@@ -40,27 +32,79 @@ async function run() {
       res.send(result);
     });
 
-
-    app.post('/createAssignment' , async(req,res)=>{
+    app.post("/createAssignment", async (req, res) => {
       const newAssignment = req.body;
-      
-      const result = await createCollection.insertOne(newAssignment) 
-      res.send(result)
-    })
 
-    app.post('/deleteAssignment/:id' , async(req , res)=>{
-      const id = req.params.id
+      const result = await createCollection.insertOne(newAssignment);
+      res.send(result);
+    });
+
+    app.post("/deleteAssignment/:id", async (req, res) => {
+      const id = req.params.id;
       const userEmail = req.body.email;
-      console.log(userEmail)
+      console.log(userEmail);
       const query = {
-        _id : new ObjectId(id),
-        email : userEmail
+        _id: new ObjectId(id),
+        email: userEmail,
+      };
+      const result = await createCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/viewAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await createCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/allAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+ 
+
+      const result = await createCollection.findOne({ _id: new ObjectId(id) });
+ 
+      res.send(result);
+    });
+
+    app.post("/submittedAssignment", async (req, res) => {
+      const newSubmit = req.body;
+
+      const { email, assignmentId } = newSubmit;
+
+      const exist = await submittedCollection.findOne({ email, assignmentId });
+      console.log(exist);
+      if (exist) {
+        return res
+          .status(409)
+          .send({ message: "you have already submitted this assignment" });
       }
-      const result = await createCollection.deleteOne(query)
-      res.send(result)
-    } )
 
+      const result = await submittedCollection.insertOne(newSubmit);
 
+      res.send(result);
+    });
+
+    app.get("/submittedAssignment", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = { email: email };
+      const result = await submittedCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.put("/updateAssignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const newAssignment = req.body;
+      console.log(newAssignment)
+      const updateDoc = {
+        $set: newAssignment,
+      };
+      const option = { upsert: true };
+      const result = await createCollection.updateOne(query, updateDoc, option);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
